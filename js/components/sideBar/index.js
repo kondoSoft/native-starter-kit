b8526c6100b655948ea7f08e762e728ddab8cfab
jblancoh <zlatanjon@hotmail.com>
2017-04-19 18:43:46 -0500

    fixed sidebar to home

--- js/components/sideBar/index.js
+++ js/components/sideBar/index.js
@@ -1,7 +1,7 @@
 
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Content, Text, List, ListItem } from 'native-base';
+import { Content, Text, List, ListItem, Icon } from 'native-base';
 
 import { setIndex } from '../../actions/list';
 import navigateTo from '../../actions/sideBarNav';
@@ -12,22 +12,33 @@ import styles from './style';
 class SideBar extends Component {
 
   static propTypes = {
-    // setIndex: React.PropTypes.func,
+    setIndex: React.PropTypes.func,
     navigateTo: React.PropTypes.func,
   }
 
   navigateTo(route) {
     this.props.navigateTo(route, 'home');
+    // console.log(route)
+
   }
 
   render() {
+    const i = this.props.navigation.index
+    // console.log("este es navigation index: " + this.props.navigation.index)
+    // console.log("este es key de la ruta:  " + this.props.navigation.routes[i].key);
     return (
       <Content style={styles.sidebar} >
           <ListItem button onPress={() => this.navigateTo('home')} >
-            <Text>Home</Text>
+            <Icon name="ios-home"/>
+            <Text style={styles.text}>Inicio</Text>
           </ListItem>
           <ListItem button onPress={() => this.navigateTo('contactus')} >
-            <Text>Contact Us</Text>
+            <Icon name="ios-mail"/>
+            <Text style={styles.text}>Contactanos</Text>
+          </ListItem>
+          <ListItem button onPress={() => this.navigateTo('contactus')} >
+            <Icon name="ios-heart"/>
+            <Text style={styles.text}>Favoritos</Text>
           </ListItem>
       </Content>
     );
